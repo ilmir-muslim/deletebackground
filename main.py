39ebef7ce0a1c57dd688aab1d6cb9890bbc321c7
ilmir <ilmir_gf@mail.ru>
2024-02-15 18:20:49 +0300

    now all file taking automaticly

--- main.py
+++ main.py
@@ -2,13 +2,13 @@ from rembg import remove
 import os
 from tqdm import tqdm
 
-directory = '/home/ilmir/photos_for_bgdel'
-
+directory = input('type the path to your folder: ')
+output_directory = input('type the path you want to save a new files: ')
 for filename in tqdm(os.listdir(directory), desc='processing...'):
 
-    name = filename.replace('.jpg','')
+    name = filename.replace('.jpg','.png')
     input_path = os.path.join(directory, filename)
-    output_path = f'/home/ilmir/Pictures/{name}.png'
+    output_path = f'{output_directory}/{name}.png'
 
     if os.path.isfile(input_path):
         with open(input_path,'rb') as i:
